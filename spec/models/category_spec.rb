c72c0031312284ea7c0c901f5c112c004317b370
Nouval Rizky <nouvalr@gmail.com>
2022-04-18 14:53:06 +0800

    Add validation for Category, invalid with duplicate name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  it "has a valid factory" do
+  it 'has a valid factory' do
     expect(FactoryBot.build(:category)).to be_valid
   end
 
@@ -13,5 +13,14 @@ RSpec.describe Category, type: :model do
     category = FactoryBot.build(:category, name: nil)
     category.valid?
     expect(category.errors[:name]).to include("can't be blank")
+  end
+
+  it 'is invalid with a duplicate name' do
+    category1 = FactoryBot.create(:category, name: 'Indonesian')
+    category2 = FactoryBot.build(:category, name: 'Indonesian')
+
+    category2.valid?
+
+    expect(category2.errors[:name]).to include('has already been taken')
   end
 end
