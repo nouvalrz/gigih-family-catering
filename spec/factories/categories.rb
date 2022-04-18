344f2ebf2b86c1c311c92d44abe09aee9f6d766e
Nouval Rizky <nouvalr@gmail.com>
2022-04-18 17:39:37 +0800

    Add validation return empty response when record nil in categories controller

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    sequence(:name) { |n| "#{Faker::Food.ethnic_category} #{n}" }
+    sequence(:name) { |n| "#{Faker::Food.ethnic_category} #{n}"[0..20] }
   end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -2,15 +2,23 @@ require 'rails_helper'
 
 RSpec.describe 'Categories', type: :request do
   describe 'GET /index' do
-    before do
-      FactoryBot.create_list(:category, 10)
-      get '/api/v1/categories'
+    context 'when category records exits' do
+      before do
+        FactoryBot.create_list(:category, 10)
+        get '/api/v1/categories'
+      end
+      it 'returns all categories' do
+        expect(json['data'].size).to eq(10)
+      end
+      it 'returns status code 200' do
+        expect(response).to have_http_status(:success)
+      end
     end
-    it 'returns all categories' do
-      expect(json['data'].size).to eq(10)
-    end
-    it 'returns status code 200' do
-      expect(response).to have_http_status(:success)
+    context 'when category records empty' do
+      it 'returns no data' do
+        expect(response).to eq(nil)
+      end
     end
   end
+
 end
