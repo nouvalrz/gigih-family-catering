580c10f311d9579ee429ed140ccc42637e18df64
Nouval Rizky <nouvalr@gmail.com>
2022-04-18 14:45:22 +0800

    Add customary spec and factory configuration for Category model

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "has a valid factory" do
+    expect(FactoryBot.build(:category)).to be_valid
+  end
+
+  it 'is valid with a name' do
+    expect(FactoryBot.build(:category)).to be_valid
+  end
 end
