9f41f7d54fa7dd774988dc471d08d9b5c979449a
Nouval Rizky <nouvalr@gmail.com>
2022-04-19 01:45:37 +0800

    Add returns error already been taken in Category#create

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::CategoriesController < ApplicationController
+  protect_from_forgery with: :null_session
   def index
     categories = Category.all
     render json: CategorySerializer.new(categories).serializable_hash.to_json
@@ -6,5 +7,20 @@ class Api::V1::CategoriesController < ApplicationController
   def show
     @category = Category.find_by(id: params[:id])
     render json: CategorySerializer.new(@category).serializable_hash.to_json
+  end
+  def create
+    category = Category.new(category_params)
+    if category.save
+      render json: CategorySerializer.new(category).serializable_hash.to_json
+    else
+      render json: ErrorSerializer.serialize(category.errors)
+    end
+  end
+
+  private
+  def category_params
+    params.require(:data)
+          .require(:attributes)
+          .permit(:name)
   end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -20,11 +20,32 @@ RSpec.describe 'Categories', type: :request do
       end
     end
   end
-  describe "GET /show" do
-    it "assigns the requested category to @category" do
+  describe 'GET /show' do
+    it 'assigns the requested category to @category' do
       category = FactoryBot.create(:category)
       get "/api/v1/categories/#{category.id}"
       expect(assigns(:category)).to eq category
+    end
+  end
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'saves the new category in the database' do
+        expect{
+          category = FactoryBot.create(:category)
+          post '/api/v1/categories', params: CategorySerializer.new(category).serializable_hash
+        }.to change(Category, :count).by(1)
+      end
+    end
+    context 'with valid parameters but already exits in database' do
+      before do
+        category = FactoryBot.create(:category, name: 'Javanese')
+        post '/api/v1/categories', params: CategorySerializer.new(category).serializable_hash
+      end
+      it 'returns error already been taken' do
+        category = FactoryBot.build(:category, name: 'Javanese')
+        post '/api/v1/categories', params: CategorySerializer.new(category).serializable_hash
+        expect(JSON.parse(response.body)['errors'][0]['title']).to eq('Name has already been taken')
+      end
     end
   end
 end
