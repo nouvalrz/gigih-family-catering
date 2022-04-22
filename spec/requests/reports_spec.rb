ba3df943630c30c780a7d83e4504e7673bc53a99
Nouval Rizky <nouvalr@gmail.com>
2022-04-23 07:08:26 +0800

    Refactor validation spec for ReportsControler#index

--- spec/requests/reports_spec.rb
+++ spec/requests/reports_spec.rb
@@ -17,9 +17,9 @@ RSpec.describe 'Reports', type: :request do
     end
     context 'with all query params' do
       before do
-        correct_order = FactoryBot.create(:order, created_at: Date.today, total_price: 1.0, customer_email: 'nouvalr@gmail.com')
-        wrong_order = FactoryBot.create(:order, created_at: Date.new(1945,11,17))
-        get "/api/v1/reports?customer_email=nouvalr@gmail.com&start_price=0&end_price=10&start_date=#{Date.today.strftime('%F')}&end_date=#{(Date.today + 1).strftime('%F')}"
+        correct_order = FactoryBot.create(:order, total_price: 1.0, customer_email: 'nouvalr@gmail.com')
+        wrong_order = FactoryBot.create(:order, total_price: 200000000)
+        get "/api/v1/reports?customer_email=nouvalr@gmail.com&start_price=0&end_price=10"
       end
       it 'returns no data' do
         expect(json['data'].size).to eq(1)
